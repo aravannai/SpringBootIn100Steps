47a0eb74edcbc4dc9ba6ca1f77a67268b4f35981
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-06 07:36:12 +0530

    Initial Commit for Spring Boot Web Application

---
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/Spring/Boot/Web/Application/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.sathish.springboot.web.Spring.Boot.Web.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Web-Application/src/test/java/com/sathish/springboot/web/Spring/Boot/Web/Application/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathish.springboot.web.Spring.Boot.Web.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
