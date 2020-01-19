74f7c73df091676574806d64bc7232c50a953695
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-19 18:46:53 +0530

    PrepareForSpringSecurityImplementation

--- Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/LoginController.java
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/LoginController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.sathish.springboot.web.service.LoginService;
@@ -17,20 +16,8 @@ public class LoginController {
 	@Autowired
 	LoginService loginService;
 
-	@RequestMapping(value="/login", method=RequestMethod.GET)
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String showLoginPage(ModelMap model ) {
-		return "login";
-	}
-	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String showWelcome(@RequestParam String name, @RequestParam String password, ModelMap model ) {
-		
-		boolean isValid = loginService.validateUser(name, password);
-		if(!isValid) {
-			model.put("errorMessage" ,"Invalid Credentials");
-			return "login";
-		}
-		model.put("name", name);
 		return "welcome";
 	}
 }
