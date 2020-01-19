5db94e09cb9d5f573d793010cfb9a7305b83401a
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-19 18:47:15 +0530

    RefactoringTodoController

--- Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/TodoController.java
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/TodoController.java
@@ -36,14 +36,18 @@ public class TodoController {
 
 	@RequestMapping(value="/list-todos", method=RequestMethod.GET)
 	public String showTodos(ModelMap model ) {
-		String name = (String)model.get("name");
+		String name = getLoggedUserName(model);
 		model.put("todos", todoService.retrieveTodos(name));
 		return "list-todos";
+	}
+
+	private String getLoggedUserName(ModelMap model) {
+		return (String)model.get("name");
 	}
 	
 	@RequestMapping(value="/add-todo", method=RequestMethod.GET)
 	public String showAddTodos(ModelMap model ) {
-		model.addAttribute("todo", new Todo(0, (String)model.get("name"), "", new Date(), false));
+		model.addAttribute("todo", new Todo(0, getLoggedUserName(model), "", new Date(), false));
 		return "todo";
 	}
 	
@@ -52,7 +56,7 @@ public class TodoController {
 		if(result.hasErrors()) {
 			return "todo";
 		}
-		todoService.addTodo((String)model.get("name"), todo.getDesc(), todo.getTargetDate(), true);
+		todoService.addTodo(getLoggedUserName(model), todo.getDesc(), todo.getTargetDate(), true);
 		return "redirect:/list-todos";
 	}
 	
@@ -76,7 +80,7 @@ public class TodoController {
 		if(result.hasErrors()) {
 			return "todo";
 		}
-		todo.setUser((String)model.get("name"));
+		todo.setUser(getLoggedUserName(model));
 		todoService.updateTodo(todo);
 		return "redirect:/list-todos";
 	}
