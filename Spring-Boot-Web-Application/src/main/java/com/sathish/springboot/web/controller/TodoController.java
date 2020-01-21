4f744f28167fd651f728bd0cfeab6948c5f8d439
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-21 07:50:08 +0530

    AddedLogoutAndRefactoringOfCode

--- Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/TodoController.java
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/TodoController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -15,13 +17,11 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.sathish.springboot.web.model.Todo;
 import com.sathish.springboot.web.service.TodoService;
 
 @Controller
-@SessionAttributes("name")
 public class TodoController {
 	
 	@Autowired
@@ -42,7 +42,11 @@ public class TodoController {
 	}
 
 	private String getLoggedUserName(ModelMap model) {
-		return (String)model.get("name");
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if(principal instanceof UserDetails) {
+			return ((UserDetails) principal).getUsername();
+		}
+		return principal.toString();
 	}
 	
 	@RequestMapping(value="/add-todo", method=RequestMethod.GET)
