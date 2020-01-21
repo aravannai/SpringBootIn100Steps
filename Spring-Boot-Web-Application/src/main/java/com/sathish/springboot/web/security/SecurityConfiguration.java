63c9014a131612c39bad07916cbdf5b4fcc92f15
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-21 07:21:20 +0530

    FixForPasswordEncryption

--- Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/security/SecurityConfiguration.java
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/security/SecurityConfiguration.java
@@ -4,16 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
 			throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(
-				org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance()).withUser("sathish").password("password")
+		auth.inMemoryAuthentication().withUser("sathish").password("{noop}password")
 				.roles("USER", "ADMIN");
 	}
 	
