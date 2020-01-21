33ccf5ff85c08920a647faf4044b4c1de697a02c
aravannai <38413795+aravannai@users.noreply.github.com>
2020-01-21 08:01:57 +0530

    ExceptionHandling

---
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/ErrorController.java
@@ -0,0 +1,20 @@
+package com.sathish.springboot.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("error")
+public class ErrorController {
+
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleException(HttpServletRequest req, Exception ex) {
+		ModelAndView mv =  new ModelAndView();
+		mv.addObject("exception", ex.getLocalizedMessage());
+		mv.addObject("url", req.getRequestURL());
+		mv.setViewName("error");
+		return mv;
+	}
+}
