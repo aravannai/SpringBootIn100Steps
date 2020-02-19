64a1fe93878337c23e2d3e39664bd142c19b23f5
aravannai <38413795+aravannai@users.noreply.github.com>
2020-02-19 07:31:26 +0530

    CHanges to Spring Boot Controller

--- Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/WelcomeNewController.java
+++ Spring-Boot-Web-Application/src/main/java/com/sathish/springboot/web/controller/WelcomeNewController.java
@@ -1,6 +1,7 @@
 package com.sathish.springboot.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,8 @@ public class WelcomeNewController {
 	//Auto wiring
 	@Autowired
 	private WelcomeService service;
+	@Value("${welome.message}")
+	private String welcomeMessage;
 
 	@RequestMapping("/welcome")
 	public String welcome() {
